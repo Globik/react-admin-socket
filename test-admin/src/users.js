57e0e8fb2e30f32ac614815aa6d9e5aa2dd312c9
Globik <gru5@yandex.ru>
2019-10-03 09:26:58 -0400

    more text fields

--- test-admin/src/users.js
+++ test-admin/src/users.js
@@ -5,6 +5,9 @@ export const UserList = props => (
 <List {...props}>
 <Datagrid rowClick="edit">
 <TextField source="id"/>
+<TextField source="email" />
+<TextField source="first_name" />
+<TextField source="last_name" />
 <ImageField source="avatar"/>
 </Datagrid>
 </List>
